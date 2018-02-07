5dc425a0ab78a3ea6ca89c71de870ad66babc385
Stavros Korokithakis <hi@stavros.io>
2018-02-08 01:35:51 +0200

    fix: Add app_name for Django 2.0

--- tokenauth/urls.py
+++ tokenauth/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = "tokenauth"
 urlpatterns = [
     url(r'^login/$', views.token_post, name="login"),
     url(r'^logout/$', views.logout, name="logout"),
