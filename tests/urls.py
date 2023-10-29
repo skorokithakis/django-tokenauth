3e4ba94658cc42cc6c21779f719dffb072dff9de
Lewis Collard <lewiscollard@gmail.com>
2023-10-29 08:31:53 +0000

    Add tests for email_login_link.

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("tokenauth.urls", namespace="tokenauth")),
+    path(
+        "authenticated/",
+        lambda request: HttpResponse(
+            "authenticated" if request.user.is_authenticated else "unauthenticated"
+        ),
+    ),
 ]
