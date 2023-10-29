9450d820b582c138eaeb6d1c326a8b707213b009
Lewis Collard <lewiscollard@gmail.com>
2023-10-29 08:52:01 +0000

    Add test for AuthToken.delete_stale.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,10 @@
-from tokenauth.models import EmailLog, generate_token
+from datetime import timedelta
+
+import pytest
+from django.utils.timezone import now
+
+from tests.factories import AuthTokenFactory
+from tokenauth.models import AuthToken, EmailLog, generate_token
 
 
 def test_emaillog_str():
@@ -14,3 +20,17 @@ def test_generate_token():
 
     # ...and that we haven't done something terminally bad.
     assert generate_token() != token
+
+
+@pytest.mark.django_db
+def test_authtoken_delete_stale():
+    expired_token = AuthTokenFactory()
+    # creating with `timestamp` kwarg doesn't actually work (probably because
+    # of auto_now_add)
+    expired_token.timestamp = now() - timedelta(days=1)
+    expired_token.save()
+    alive_token = AuthTokenFactory()
+
+    AuthToken.delete_stale()
+    assert AuthToken.objects.count() == 1
+    assert AuthToken.objects.get() == alive_token
