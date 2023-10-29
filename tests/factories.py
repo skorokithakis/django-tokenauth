028a14dc712cc453118e5d0caa1cf268cd5c0e40
Lewis Collard <lewiscollard@gmail.com>
2023-10-29 07:29:52 +0000

    Add tests for admin.

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,8 @@
 import factory
 from django.contrib.auth import get_user_model
 
+from tokenauth.models import AuthToken
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -9,3 +11,10 @@ class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence(lambda i: f"user{i}")
     email = factory.Sequence(lambda i: f"imaginary{i}@example.invalid")
     password = factory.django.Password("password")
+
+
+class AuthTokenFactory(factory.django.DjangoModelFactory):
+    email = factory.Sequence(lambda i: f"imaginary{i}@example.invalid")
+
+    class Meta:
+        model = AuthToken
--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("auth/", include("tokenauth.urls", namespace="tokenauth")),
 ]
